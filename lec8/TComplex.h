9d754a7b91fb1286ef91446556aa051a3a26580a
lisnyak <lisnyak>
2020-11-05 12:08:24 +0200

    small updates

--- lec8/TComplex.h
+++ lec8/TComplex.h
@@ -3,31 +3,27 @@
 #ifndef TComplexH
 #define TComplexH
 #include <iostream.h>
+
 class MyInt{
-public:
-  int data;
+  public:
+    int data;
 };
+
 class TComplex {
    private:
       double re;
       double im;
    public:
-       int * data;
+   TComplex(double _re):re(_re),im(0){
+     cout << "Convert costructor was called" << endl;
+   }
+   TComplex():re(0),im(0){
+    cout << "Empty Costructor was called" << endl;
+   }
+   TComplex(double _re, double _im):re(_re),im(_im){
+    cout << "Init costructor was called" << endl;
+   }
 
-   explicit TComplex(double _re = 0, double _im = 0):re(_re),im(_im){
-        data = new int;
-        *data = 10;
-   }
-   friend TComplex operator + (const TComplex&, const TComplex&);
-   //friend TComplex operator + (const double&, const TComplex&);
-   friend ostream& operator << (ostream& out , const TComplex& c);
-
-   void print() const {
-    cout << "Print const" << endl;
-   }
-   void print() {
-    cout << "Print" << endl;
-   }
    void setRe(double val){
     re = val;
    }
@@ -35,71 +31,85 @@ class TComplex {
     im = val;
    }
    double getRe() const{
-   //re = 5;
     return re;
    }
    double getIm() const{
     return im;
    }
-   	TComplex operator - (const TComplex& c){
-		    return TComplex(this->re - c.re, this->im - c.im);
-		};
-      TComplex operator + (const double& a){
-		    return TComplex(this->re + a, this->im);
-		};
 
-		TComplex operator = (const TComplex& c){
+   friend TComplex operator + (const TComplex&, const TComplex&);
+   friend TComplex operator + (const double&, const TComplex&);
+   friend TComplex operator + (const TComplex&, const double&);
+
+
+   friend ostream& operator << (ostream& out , const TComplex& c);
+
+   TComplex operator - (const TComplex& c){
+		    return TComplex(this->re - c.re, this->im - c.im);
+	 };
+
+   /*TComplex operator + (const double& a){
+		    return TComplex(this->re + a, this->im);
+	 };         */
+
+	 TComplex operator = (const TComplex& c){
+      cout << "operator = (const TComplex& c)"<< endl;
 			this->re = c.re;
 			this->im = c.im;
-         *(this->data) = *(c.data)  ;
-		}
-      TComplex operator = (const double c){
+	 }
+
+   /*TComplex operator = (const double c){
+      cout << "operator = (const double c)"<< endl;
 			this->re = c;
 			this->im = 0;
+   }*/
 
-		}
-		TComplex operator ++ (){
+	 TComplex operator ++ (){
 			this->re++;
-         cout << "operator ++";
+      cout << "operator ++";
 			return *(this);
-		}
-		TComplex operator ++ (int){
-         cout << "operator ++ int";
+	 }
+	 TComplex operator ++ (int){
+      cout << "operator ++ int";
 			TComplex res =  *(this);
 			this->re++;
 			return res;
-		}
-      double operator [](const int ind){
+	 }
+
+   double operator [](const int ind){
 			if (ind == 0) return this->re;
 			return this->im;
-		}
-      TComplex operator ()(double _re, double _im ){
+	 }
+
+   TComplex operator ()(double _re = 0 , double _im = 0 ){
         this->re = _re;
         this->im = _im;
         return *this;
-		}
-      operator double () {
+	 }
+
+   operator double () {
 		 	return re;
-		}
+	 }
 
-
-      operator MyInt () {
-         MyInt a;
-         a.data = this->re;
-		 	return a;
-		}
-
+   operator MyInt () {
+      MyInt a;
+      a.data = this->re;
+    	return a;
+	 }
 };
 
 TComplex operator + (const TComplex & a , const TComplex& b){
    return TComplex(a.re + b.re, a.im + b.im);
 };
-/*TComplex operator + (const double & b , const TComplex& a){
+TComplex operator + (const double & b , const TComplex& a){
    return TComplex(a.re + b, a.im );
-};*/
+};
+TComplex operator + (const TComplex& a, const double & b){
+   return TComplex(a.re + b, a.im );
+};
 
 ostream& operator << (ostream& out , const TComplex& c){
-   return  out << "("<<  c.re << "; "<< c.im <<";"<< *(c.data)  <<")";
+   return  out << "("<<  c.re << "; "<< c.im << ")";
 } ;
 //---------------------------------------------------------------------------
 #endif
